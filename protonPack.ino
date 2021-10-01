d9d599a9b5404ff01af07713a620fe9645e01093
duffer <60157909+duff0013@users.noreply.github.com>
2021-09-30 23:24:17 -0500

    Added cyclotron lights but need to fix the animation

--- protonPack.ino
+++ protonPack.ino
@@ -4,14 +4,13 @@
 #define POWERCELL_PIN 2
 #define CYCLOTRON_PIN 3
 
-// How many NeoPixels are attached?
-//#define POWERCELL_COUNT 16
-//#define CYCLOTRON_COUNT 4
-
+/************** Animation Speeds ********************/
+int pwrInterval = 45;               // powercell animation speed
+int cycloInterval = 400;            // cyclotron animation speed
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use
 Adafruit_NeoPixel powercell(16, POWERCELL_PIN, NEO_GRB + NEO_KHZ800);
-//Adafruit_NeoPixel cyclotron(CYCLOTRON_COUNT, CYCLOTRON_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel cyclotron(4, CYCLOTRON_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   // Initialize powercell LEDS to 'off' and set brightness
@@ -19,19 +18,62 @@ void setup() {
   powercell.show();
   powercell.setBrightness(100);
 
+  cyclotron.begin();
+  cyclotron.show();
+  cyclotron.setBrightness(255);
 }
 
 /************************* Main **********************/
-int pwrInterval = 45;               // powercell cycle speed
 void loop() {
   int currentMillis = millis();
+  
   powerCell_normalMode(currentMillis, pwrInterval);
+  cyclotron_normalMode(currentMillis, cycloInterval);
+//cyclotron.setPixelColor(0, cyclotron.Color(0,150,0));
+//cyclotron.setPixelColor(1, cyclotron.Color(0,150,0));
+//cyclotron.setPixelColor(2, cyclotron.Color(0,150,0));
+//cyclotron.setPixelColor(3, cyclotron.Color(0,150,0));
+//cyclotron.show();
 }
 /************************ End Main ***************************/
 
+/****************** Cyclotron Animation *********************/
+unsigned long prevCycloMillis = 0;
 
-/****************** Powercell Animations *********************/
-//int pwrInterval = 1000;               // powercell cycle speed
+int cycloSeqTotal = 4;
+int cycloSeqNum = 0;
+
+void cyclotron_normalMode(int currentMillis, int anispeed)
+{
+ if(currentMillis - prevCycloMillis > anispeed)
+ {
+  // save the last time we blinked a cyclotron led
+  prevCycloMillis = currentMillis;
+
+  for(int i=0; i<cycloSeqTotal; i++)
+  {
+    if(i <= cycloSeqNum)
+    {
+      cyclotron.setPixelColor(i, cyclotron.Color(150,0,0));
+    }
+    else
+    {
+      cyclotron.setPixelColor(i, 0);
+    }
+  }
+  cyclotron.show();
+  if(cycloSeqNum < cycloSeqTotal)
+  {
+    cycloSeqNum++;
+  }
+  else
+  {
+    cycloSeqNum = 0;
+  }
+ }
+}
+
+/****************** Powercell Animation *********************/
 unsigned long prevPwrMillis = 0;      // last time we changed a light in the sequence
 
 int powerSeqTotal = 16;               // number of led's in powercell
@@ -41,7 +83,7 @@ void powerCell_normalMode(int currentMillis, int anispeed)
 {
   if(currentMillis - prevPwrMillis > anispeed)
   {
-    // save the last time we blinked an led
+    // save the last time we blinked a powercell led
     prevPwrMillis = currentMillis;
 
     for(int i=0; i<powerSeqTotal; i++)
